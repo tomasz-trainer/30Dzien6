05aef72613f4849d8864aa309358272fdd9a2067
tomasz <csharp@406-00>
2026-07-06 11:11:40 +0200

    Add P02PolaczenieZBaza project with SQL connection class
    Added new console project P02PolaczenieZBaza to the solution, targeting .NET Framework 4.7.2. Included App.config, project file, and assembly metadata. Implemented PolaczenieZBaza class for connecting to SQL Server and retrieving data from the "Zawodnicy" table. Added a basic Program.cs entry point.

---
+++ P02PolaczenieZBaza/PolaczenieZBaza.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02PolaczenieZBaza
+{
+    internal class PolaczenieZBaza
+    {
+        private readonly string connectionString;
+
+        public PolaczenieZBaza()
+        {
+            connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=BazaSportowa;Integrated Security=True";
+        }
+
+        public PolaczenieZBaza(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public object[][] WyslijPolecenieSQL(string sql)
+        {
+            SqlConnection connection;// nawiazywanie polaczenia z baza 
+            SqlCommand command; // przechowywanie polecen SQL 
+            SqlDataReader sqlDataReader; // czytanie wyników z bazy
+             
+            connection = new SqlConnection(connectionString);
+
+            command = new SqlCommand("select * from Zawodnicy", connection);
+
+            connection.Open();
+
+            sqlDataReader = command.ExecuteReader();
+
+
+            int liczbakolumn = sqlDataReader.FieldCount;
+            List<object[]> listaWierszy = new List<object[]>();
+
+
+            while (sqlDataReader.Read())
+            {
+                object[] komorki = new object[liczbakolumn];
+                for (int i = 0; i < liczbakolumn; i++)
+                    komorki[i] = sqlDataReader.GetValue(i);
+
+                listaWierszy.Add(komorki);
+            }
+
+            connection.Close();
+            return listaWierszy.ToArray();
+
+        }
+    }
+}
